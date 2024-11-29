670ad7fc3abc66fa9754bdbfd17ea9070f6bbbe2
MerrCraft <161960686+MerrCraft@users.noreply.github.com>
2024-11-28 21:44:27 -0600

    use the saying"never reapeat code"

--- script.js
+++ script.js
@@ -6,3 +6,6 @@ function setFavicons(favImg){
     headTitle.appendChild(setFavicon);
 }
 setFavicons('favicon.png');
+
+const nav = document.getElementById("nav");
+nav.innerHTML = "<img src="TheImage.png" alt="This image cannot displayed" /><h2>The Forum<br> <a href="Howto.html" class="link">Howto</a><a href="Forum.html" class="link">Forum</a></h2>";
