6061c21793f0bc74e24233ff94b59bb94ca7f264
MerrCraft <161960686+MerrCraft@users.noreply.github.com>
2024-11-28 22:02:38 -0600

    Sadly, i had to reapeat code (will fix in future)

--- script.js
+++ script.js
@@ -7,5 +7,3 @@ function setFavicons(favImg){
 }
 setFavicons('favicon.png');
 
-const gog = document.getElementsByClassName("header");
-gog.innerHTML = "<img src="TheImage.png" alt="This image cannot displayed" /><h2>The Forum<br> <a href="Howto.html" class="link">Howto</a><a href="Forum.html" class="link">Forum</a></h2>";
