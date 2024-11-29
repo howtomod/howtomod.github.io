e45cc4881232abfad73cc76bc1807e7e60213d9c
MerrCraft <161960686+MerrCraft@users.noreply.github.com>
2024-11-28 21:46:52 -0600

    Well never reapeat code is still on

--- script.js
+++ script.js
@@ -7,5 +7,5 @@ function setFavicons(favImg){
 }
 setFavicons('favicon.png');
 
-const nav = document.getElementById("nav");
-nav.innerHTML = "<img src="TheImage.png" alt="This image cannot displayed" /><h2>The Forum<br> <a href="Howto.html" class="link">Howto</a><a href="Forum.html" class="link">Forum</a></h2>";
+const gog = document.getElementById("nav");
+gog.innerHTML = "<img src="TheImage.png" alt="This image cannot displayed" /><h2>The Forum<br> <a href="Howto.html" class="link">Howto</a><a href="Forum.html" class="link">Forum</a></h2>";
